66324cc546d771d04b55af9e318bb4ac0db1b242
Christian Vogel <vogelchr@vogel.cx>
2020-03-01 12:43:19 +0000

    typos

--- include/pet_kbd.h
+++ include/pet_kbd.h
@@ -17,7 +17,7 @@
 #define PET_KEY_A(v) (0x00 | (0x0f & ((v) + 1)))
 #define PET_KEY_B(v) (0x10 | (0x0f & ((v) + 1)))
 #define PET_KEY_C(v) (0x20 | (0x0f & ((v) + 1)))
-#define PET_KEY_D(v) (0x33 | (0x0f & ((v) + 1)))
+#define PET_KEY_D(v) (0x30 | (0x0f & ((v) + 1)))
 #define PET_KEY_E(v) (0x40 | (0x0f & ((v) + 1)))
 #define PET_KEY_F(v) (0x50 | (0x0f & ((v) + 1)))
 #define PET_KEY_G(v) (0x60 | (0x0f & ((v) + 1)))
